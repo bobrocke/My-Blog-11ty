c312afcac059c70d3f54a55857576602ed181832
Bob Rockefeller <bob@bobrockefeller.com>
2025-03-26 21:46:51 -0400

    Removed Tailwind via postcss
    Browser reload was not working that way.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,17 +1,10 @@
 import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import eleventyPluginMarkdown from "@jgarber/eleventy-plugin-markdown";
 
-import fs from "fs";
-import path from "path";
-
-import cssnano from "cssnano";
-import postcss from "postcss";
-import tailwindcss from "@tailwindcss/postcss";
-
 export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets", {
     // Don't copy the tailwind-input.css file into _site
-    filter: ["!**/*tailwind-input.css"],
+    filter: ["!**/*tailwind.css"],
   });
 
   // Create a collection of all the posts in src/posts.
@@ -23,37 +16,6 @@ export default async function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
   // And the markdown filter plugin
   eleventyConfig.addPlugin(eleventyPluginMarkdown);
-
-  //compile tailwind before eleventy processes the files
-  eleventyConfig.on("eleventy.before", async () => {
-    const tailwindInputPath = path.resolve("./src/assets/css/tailwind.css");
-
-    const tailwindOutputPath = "./_site/assets/css/tailwind.css";
-
-    const cssContent = fs.readFileSync(tailwindInputPath, "utf8");
-
-    const outputDir = path.dirname(tailwindOutputPath);
-    if (!fs.existsSync(outputDir)) {
-      fs.mkdirSync(outputDir, { recursive: true });
-    }
-
-    const result = await processor.process(cssContent, {
-      from: tailwindInputPath,
-      to: tailwindOutputPath,
-    });
-
-    fs.writeFileSync(tailwindOutputPath, result.css);
-  });
-
-  const processor = postcss([
-    //compile tailwind
-    tailwindcss(),
-
-    //minify tailwind css
-    cssnano({
-      preset: "default",
-    }),
-  ]);
 }
 
 export const config = {
